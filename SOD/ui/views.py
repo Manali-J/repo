55b6e9b60a8cd1141f80e203acd88335e792b93b
Manali <Manali-J@users.noreply.github.com>
2018-11-28 20:09:04 +0530

    Delete views.py

--- SOD/ui/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-#from . import models
-from api import models
-
-class HomeView(ListView):
-	template_name = 'ui/home.html'
-	queryset = models.IMOS.objects.all()
